efc631c2c11cd59d32c526a100ef7eb528ed1575
Ganglians <jchav027@ucr.edu>
2019-06-24 08:13:45 -0700

    Prompt for phrase and determine if it's palindromic

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 let Phrase = require("ganglians-palindrome");
+let string = prompt("Please enter a string for palindrome testing:");
 
-alert(new Phrase("Madam, I'm Adam.").palindrome());
+let phrase = new Phrase(string);
+
+if(phrase.palindrome()) {
+  alert(`"${phrase.content}" is a palindrome!`);
+}
+else {
+  alert(`"${phrase.content}" is not a palindrome.`);
+}
